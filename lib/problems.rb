e8aaf72c84bb7a7d5f68ab982e334a6255fafe9a
“Spencer <Spencer@bookservo.com>
2023-08-31 15:15:45 -0400

    Updated Recap Porject

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,14 +1,27 @@
 # Write a method, least_common_multiple, that takes in two numbers and returns
 # the smallest number that is a mutiple of both of the given numbers
 def least_common_multiple(num_1, num_2)
-  
+  (1..num_1 * num_2).each do |num|
+    if num % num_1 == 0 && num % num_2 == 0
+      return num
+    end
+  end
 end
 
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the
 # two adjacent letters that appear the most in the string.
 def most_frequent_bigram(str)
-  
+  i = 0
+  pairs = Hash.new(0)
+  while i < str.length - 1
+    pair = str[i..i + 1]
+    pairs[pair] += 1
+    i += 1
+  end
+
+  sorted = pairs.sort_by { |k, v| v }
+  sorted.last[0]
 end
 
 
@@ -16,7 +29,12 @@ class Hash
   # Write a method, Hash#inverse, that returns a new hash where the key-value
   # pairs are swapped
   def inverse
-    
+    new_hash = {}
+
+    self.each do |k, v|
+      new_hash[v] = k
+    end
+    new_hash
   end
 end
 
@@ -25,7 +43,15 @@ class Array
   # Write a method, Array#pair_sum_count, that takes in a target number returns
   # the number of pairs of elements that sum to the given target
   def pair_sum_count(num)
-    
+    count = 0
+    self.each.with_index do |ele_1, idx_1|
+      self.each.with_index do |ele_2, idx_2|
+        if idx_2 > idx_1 && ele_1 + ele_2 == num
+          count += 1
+        end
+      end
+    end
+    count
   end
 
 
@@ -43,6 +69,19 @@ class Array
   #
   # This should remind you of the spaceship operator! Convenient :)
   def bubble_sort(&prc)
-    
+    prc ||= Proc.new{ |a, b| a <=> b}
+
+    sorted = false
+
+    while !sorted 
+      sorted = true
+      (0...self.length - 1).each do |i|
+        if prc.call(self[i], self[i + 1]) == 1
+          self[i], self[i + 1] = self[i + 1], self[i]
+          sorted = false
+        end
+      end
+    end
+    self
   end
 end
